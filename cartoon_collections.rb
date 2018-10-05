02be0b97c3768ef102fe1912c7f33446937dc9e4
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-05 03:34:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,13 @@
 def roll_call_dwarves(array)
 x = 1
 array.each do |elem|
-  puts "#{x} #{elem}" 
+  puts "#{x} #{elem}" + "\n"
   x+=1
 end
 end
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.each do |elem|
+    
 end
 
 def long_planeteer_calls# code an argument here
